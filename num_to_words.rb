6c01b9653322222f41baaf2b6672587663dc2d0d
shijithraghav <srk6kichu@gmail.com>
2019-01-27 20:25:12 +0530

    complete logic

--- num_to_words.rb
+++ num_to_words.rb
@@ -5,39 +5,23 @@ class NumToWords
   LETTER_WORDS = {"2"=>["a","b","c"],"3"=>["d","e","f"],"4"=>["g","h","i"],"5"=>["j","k","l"],"6"=>["m","n","o"],"7"=>["p","q","r","s"],"8"=>["t","u","v"],"9"=>["w","x","y","z"]}
 
 	def initialize
-    @all_words = {"10"=>[]}
-		(3..7).each do|k|
-			@all_words[k.to_s] = []
-		end
+    @all_words = {"3"=>[],"4"=>[],"5"=>[],"6"=>[],"7"=>[],"10"=>[]}
 		File.foreach("dictionary.txt") do|x|
-      if @all_words.keys.include?(x.gsub("\n","").length.to_s)
-        @all_words[x.gsub("\n","").length.to_s] << x.gsub("\n","")
+      word = x.gsub("\n","")
+      len = word.length.to_s
+      if @all_words.keys.include?(len)
+        @all_words[len] << word.downcase
       end
     end
   end
 
-  def find_matching_words(num)
+  def find_matching_words(num,detailed=false)
 		matching_words=Hash.new
 		final_array = Array.new
     num_a = num.split("")
     @all_words.keys.each do |k|
       matching_words[k] = return_matching_words(num_a[0..(k.to_i - 1)])
     end
-#    all_words.each_pair do|k,v|
-#      matching_words[k]=[]
-#      #num_a[0..(i-1)]
-#      v.each do|w|
-#        matched=true
-#        w_a = w.split("")
-#        w_a.each_with_index do|l,i|
-#          unless letter_words[num_a[i]].include?(l.downcase)
-#            matched = false
-#            break
-#          end
-#        end
-#        matching_words[k] << w if matched==true
-#      end
-#    end
     matching_words.each_pair do|m,n|
       unless n.empty?
         remaining_length = 10 - m.to_i
@@ -48,26 +32,44 @@ class NumToWords
               comp_words.map{|c| final_array.push([f_w,c])}
             end
           end
-#          all_words[remaining_length.to_s].each do|w|
-#            matched=true
-#            num_b = num_a[(m.to_i)..9]
-#            w_a = w.split("")
-#            w_a.each_with_index do|l,i|
-#              unless letter_words[num_b[i]].include?(l.downcase)
-#                matched = false
-#                break
-#              end
-#            end
-#            if matched == true
-#              n.map{|w_m| final_array << [w_m,w]}
-#            end
-#
-#          end
-          if remaining_length >= 6
-
+          if detailed == true
+            if remaining_length == 6
+              s_comp_words = return_matching_words(num_a[4..6])
+              unless s_comp_words.empty?
+                t_comp_words = return_matching_words(num_a[7..9])
+                unless t_comp_words.empty?
+                  n.each do|f_w|
+                    s_comp_words.each do|s|
+                      t_comp_words.map{|t| final_array.push([f_w,s,t]) }
+                    end
+                  end
+                end
+              end
+            elsif remaining_length == 7
+              [3,4].each do |y|
+                s_comp_words = return_matching_words(num_a[3..(2 + y)])
+                unless s_comp_words.empty?
+                  t_comp_words = return_matching_words(num_a[(2+y)..9])
+                  unless t_comp_words.empty?
+                    n.each do|f_w|
+                      s_comp_words.each do|s|
+                        t_comp_words.map{|t| final_array.push([f_w,s,t]) }
+                      end
+                    end
+                  end
+                end
+              end
+            end
           end
         else
           n.map{|w_m| final_array << w_m}
+        end
+      end
+    end
+    unless matching_words["10"].empty?
+      final_array.each do|f|
+        if f.class.name == "Array"
+          final_array.delete(f) if matching_words["10"].include?(f.join(""))
         end
       end
     end
@@ -80,7 +82,7 @@ class NumToWords
       matched=true
       w_a = w.split("")
       w_a.each_with_index do|l,i|
-        unless LETTER_WORDS[num_array[i]].include?(l.downcase)
+        unless LETTER_WORDS[num_array[i]].include?(l)
           matched = false
           break
         end
